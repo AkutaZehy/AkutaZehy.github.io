f2c4a5947fbd4ad6f28ea235a5bbfbc852b1217d
Akuta Zehy <akuta_zehy@outlook.com>
2024-07-13 00:15:31 +0800

    3.1.1 release

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 function welcome() {
   let num = Math.random();
-  num > 0.9
-    ? window.alert("休憩中よ。")
-    : num > 0.8 && window.alert("今日もいい天気！");
+  num > 0.99
+    ? window.alert("今日もいい天気！")
+    : num > 0.95 && window.alert("休憩中よ。");
 }
 
 function loadCode(filename) {
