7d010b811c7ec6df5feb2afabed1e3988857db0b
Akuta Zehy <akuta_zehy@outlook.com>
2024-09-22 11:31:35 +0800

    feat(wiggle-text): add wiggle-text

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ function welcome() {
   let num = Math.random();
   num > 0.99
     ? window.alert("今日もいい天気！")
-    : num > 0.5 && window.alert("休憩中よ。");
+    : num > 0.9 && window.alert("休憩中よ。");
 }
 
 function loadCode(filename) {
