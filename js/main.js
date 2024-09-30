1e32ad05855fccea30b564a1e96e679adc68c8e5
Akuta Zehy <akuta_zehy@outlook.com>
2024-09-30 09:41:24 +0800

    feat(about): add about page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-function welcome() {
-  let num = Math.random();
-  num > 0.99
-    ? window.alert("今日もいい天気！")
-    : num > 0.9 && window.alert("休憩中よ。");
+function welcome (threshold) {
+  let num = threshold;
+  if (num > 0.99) {
+    window.alert("今日もいい天気！");
+  } else if (num > 0.9) {
+    window.alert("休憩中よ。");
+  }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const audio = document.querySelector('audio');
+  const num = Math.random();
+
+  let isPlaying = false;
+  welcome(num);
+
+  document.addEventListener('click', () => {
+    if (!isPlaying && num > 0.8) {
+      audio.play();
+      isPlaying = true;
+      console.log('HELLO');
+    }
+  });
+});
